144f37020c084a8ae187c2e2a27ac38e66e2c27c
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-09-24 20:20:57 +0300

    Step_3

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express')
 const server = express()
 const port = 8080;
 
+
 server.get('/', (request, response) => {
   response.send("ok");
 });
 
-server.listen(
-  port,
-   () => console.log(`Server listening on port ${port}.`));
+server.get('/test', (request, response) => {
+  response.json({status:200, message:"ok"});
+});
+
+server.get('/time', (request, response) => {
+  const dateTime = new Date();
+  response.json({status:200, message:`${dateTime.getHours()}:${dateTime.getMinutes()}`});
+});
+
+server.listen( port,() =>  {
+console.log(`Server listening on port ${port}.`);});
