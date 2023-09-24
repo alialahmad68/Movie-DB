c5061cd88ffe898434565cdfb58d97816e776ba7
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-09-24 21:25:19 +0300

    Step_4

--- index.js
+++ index.js
@@ -16,5 +16,20 @@ server.get('/time', (request, response) => {
   response.json({status:200, message:`${dateTime.getHours()}:${dateTime.getMinutes()}`});
 });
 
+server.get(`/hello/:id?`,(request, response) => {
+  const { id } = request.params; 
+  response.json({status:200, message:`Hello, ${id? id : "anonymous"}`});
+});
+
+server.get(`/search`, (request, response) => {
+  const {s} = request.query;
+
+  if (s) {
+    response.status(200).json({ status: 200, message: 'ok', data:s});
+  } else {
+    response.status(500).json({ status: 500, error: true, message: 'you have to provide a search' });
+  }
+});
+
 server.listen( port,() =>  {
 console.log(`Server listening on port ${port}.`);});
