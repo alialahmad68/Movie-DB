b88272f22a3050b8f61a8290fc1c2ca567181405
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-09-24 21:47:40 +0300

    Step_5

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const server = express()
 const port = 8080;
 
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
 
 server.get('/', (request, response) => {
   response.send("ok");
@@ -29,6 +36,22 @@ server.get(`/search`, (request, response) => {
   } else {
     response.status(500).json({ status: 500, error: true, message: 'you have to provide a search' });
   }
+});
+
+server.get(`/movies/create`,(request, response) => {
+  response.send("create movie");
+});
+
+server.get(`/movies/read`,(request, response) => {
+  response.json({status:200, data:movies});
+});
+
+server.get(`/movies/update`,(request, response) => {
+  response.send("update movie");
+});
+
+server.get(`/movies/delete`,(request, response) => {
+  response.send("delete movie");
 });
 
 server.listen( port,() =>  {
